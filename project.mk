9404d0a58ca6ebbbfb2a88c485078ea06027c718
Tobias Kässer <tobias.kasser@upbound.io>
2025-03-06 10:06:41 +0100

    Add project.mk and update build-submodule

---
+++ project.mk
@@ -0,0 +1,4 @@
+PROJECT_NAME := configuration-gcp-gke-workload-identity
+UPTEST_INPUT_MANIFESTS := examples/network-xr.yaml,examples/gke-xr.yaml,examples/workload-identity-xr.yaml
+UPTEST_SKIP_UPDATE := true
+UPTEST_SKIP_IMPORT := true
